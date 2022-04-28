c40a2e394fde15e57386c1c96898bc69441939df
‘qianxi’ <3112560244@qq.com>
2022-04-28 12:39:10 +0800

    Initial commit

--- src/main/java/com/qx/controller/TestController.java
+++ src/main/java/com/qx/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.qx.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -16,10 +18,24 @@ public class TestController {
         return "login";
     }
 
+//    @RequestMapping(value = "/login",method = RequestMethod.POST)
+//    public String login(HttpServletRequest request, HttpSession session){
+//        String name = request.getParameter("name");
+//        String pwd = request.getParameter("pwd");
+//        if("admin".equals(name) && "admin".equals(pwd)){
+//            session.setAttribute("name",name);
+//            request.setAttribute("msg","登陆成功");
+//        }else{
+//            request.setAttribute("msg","登陆失败");
+//        }
+//
+//        return "info";
+//    }
+
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(HttpServletRequest request, HttpSession session){
-        String name = request.getParameter("name");
-        String pwd = request.getParameter("pwd");
+    public String login(@ModelAttribute(value = "name") String name, @ModelAttribute(value = "pwd") String pwd, HttpSession session, HttpServletRequest request){
+//        String name = request.getParameter("name");
+//        String pwd = request.getParameter("pwd");
         if("admin".equals(name) && "admin".equals(pwd)){
             session.setAttribute("name",name);
             request.setAttribute("msg","登陆成功");
@@ -29,7 +45,6 @@ public class TestController {
 
         return "info";
     }
-
 
 
 }
